c59ff93f5d039002964dc8ee431735ab6c585cdb
Wei-Han Hsu <ddman821101@gmail.com>
2020-12-17 13:16:54 +0800

    Add files via upload

--- Data_preprocessing-1.py
+++ Data_preprocessing-1.py
@@ -102,7 +102,7 @@ for i in range(len(final_all_path_and_labels)):
         all_labels.append(final_all_path_and_labels[i][1][j])
 
 pd.DataFrame(all_labels).value_counts().to_csv("original_sum_of_categories.csv", sep="\t") # Save the sum of categories of labels data
-
+#========================================================================================================================================
 # The labels we want (1)
 rest_labels = ['Verse', 'Silence', 'Chorus', 'End', 'Outro', 'Intro', 'Bridge', 'Intrumental', 'Interlude', 'Fade-out',\
                 'Solo', 'Pre-Verse', 'silence', 'Pre-Chorus', 'Head', 'Coda', 'Theme', 'Transition',\
@@ -116,9 +116,38 @@ for i in range(len(all_labels)):
         else :
             rest_all_labels = rest_all_labels
 
-pd.DataFrame(rest_all_labels).value_counts().to_csv("new_sum_of_categories.csv", sep="\t")
+# merge the same labels (ex: outro and Outro)
+for i in range(len(rest_all_labels)):
+    if rest_all_labels[i] == 'silence':
+        rest_all_labels[i] = 'Silence'
+    elif rest_all_labels[i] == 'outro':
+        rest_all_labels[i] = 'Outro'
+    elif rest_all_labels[i] == 'Secondary_theme':
+        rest_all_labels[i] = 'Secondary_Theme'
+    else :
+        rest_all_labels[i] = rest_all_labels[i]
 
-# 
+pd.DataFrame(rest_all_labels).value_counts().to_csv("new_sum_of_categories.csv", sep="\t")
+#========================================================================================================================================
+# The labels we want (2)
+rest_labels_2 = ['Verse', 'Chorus', 'Outro', 'Intro', 'Bridge', 'Intrumental',\
+                'Pre-Verse', 'Pre-Chorus', 'outro']
+
+rest_all_labels_2 = []
+for i in range(len(all_labels)):
+    for j in range(len(rest_labels_2)):
+        if all_labels[i] == rest_labels_2[j]:
+            rest_all_labels_2.append(all_labels[i])
+        else :
+            rest_all_labels_2 = rest_all_labels_2
+
+for i in range(len(rest_all_labels_2)):
+    if rest_all_labels_2[i] == 'outro':
+        rest_all_labels_2[i] = 'Outro'
+    else :
+        rest_all_labels_2[i] = rest_all_labels_2[i]
+
+pd.DataFrame(rest_all_labels_2).value_counts().to_csv("new_sum_of_categories_2.csv", sep="\t")
 #=======================================================================================================================================
 # rest_labels = ['Verse', 'Silence', 'Chorus', 'End', 'Outro', 'Intro', 'Bridge', 'Intrumental', 'Interlude', 'Fade-out',\
 #                'Solo', 'Pre-Verse', 'silence', 'Pre-Chorus', 'Head', 'Coda', 'Theme', 'Transition',\
@@ -229,331 +258,3 @@ for i in range(len(all_seg)):
         new_seg = new_seg
     else:
         new_seg.append(all_seg[i])
-
-
-
-#=======================================================================================================================================
-# l_list = []
-# for i in range(len(patch_with_labels_list)):
-#     l_list.append(patch_with_labels_list[i][0])
-# len(np.unique(l_list)) # all category number
-
-# for i in range(len(patch_with_labels_list)):
-#     print(np.shape(patch_with_labels_list[i][1]))
-
-# long = len(patch_with_labels_list)
-
-# # find all under the 3 seconds patch and delete it !!!
-# over_short = []
-# for i in range(long):
-#     if np.shape(patch_with_labels_list[i][1])[1] < 94 :
-#         over_short.append(i)
-#     else :
-#         over_short = over_short
-
-# for i in over_short:
-#     print(np.shape(patch_with_labels_list[i][1]))
-#=========================================================================================================================================
-# fill all the long enough patches into the new list and crack it into 3 seconds patches
-p_l = []
-for i in range(len(new_seg)):
-    if np.shape(new_seg[i][3])[1] >= 94 :
-        # crack it into 3 seconds patches
-        for j in range(0, np.shape(new_seg[i][3])[1], 32):
-            l = []
-            if np.shape(new_seg[i][3])[1] >= 94+j :
-                temp_array = new_seg[i][3][0:128,0+j:94+j]
-                l.append(new_seg[i][2])
-                l.append(temp_array)
-                p_l.append(l)
-            else :
-                p_l = p_l
-    else:
-        p_l = p_l
-# aa = 0
-# for i in range(len(p_l)):
-#     aa += len(p_l[i][1])
-# print(aa) # Total the patches number
-# to_l = []
-# for i in range(len(p_l)):
-#     to_l.append(p_l[i][0])
-# len(np.unique(to_l)) # Total the patches labels number
-# targets_num = pd.get_dummies(pd.Series(np.unique(to_l))) # Get the labels to the number(arrays)
-
-# shuffle and split the data set to test set and train set
-random.shuffle(p_l)
-training_p_l, testing_p_l = sklearn.model_selection.train_test_split(p_l, train_size=len(p_l)-10000, test_size=10000)
-
-# set the labels and data (training)
-train_d = []
-train_t = []
-for i in range(len(training_p_l)):
-    train_d.append(training_p_l[i][1])
-    train_t.append(training_p_l[i][0])
-train_d = np.array(train_d)
-train_t = np.array(train_t)
-
-# set the labels and data (testing)
-test_d = []
-test_t = []
-for i in range(len(training_p_l)):
-    test_d.append(training_p_l[i][1])
-    test_t.append(training_p_l[i][0])
-test_d = np.array(test_d)
-test_t = np.array(test_t)
-
-# set the labels to the number
-targets_num_train = pd.get_dummies(pd.Series(np.unique(train_t)))
-targets_num_test = pd.get_dummies(pd.Series(np.unique(test_t)))
-
-# Set the labels and data well !!!
-train_targets = []
-for i in range(len(train_t)):
-    train_targets.append(np.argmax(np.array(targets_num_train[train_t[i]])))
-train_targets = np.array(train_targets)
-test_targets = []
-for i in range(len(test_t)):
-    test_targets.append(np.argmax(np.array(targets_num_test[test_t[i]])))
-test_targets = np.array(test_targets)
-
-x_val = train_d[:10000]
-y_val = train_targets[:10000]
-
-train_d = train_d[10000:]
-train_targets = train_targets[10000:]
-
-training_dataset = tf.data.Dataset.from_tensor_slices((train_d, train_targets))
-testing_dataset = tf.data.Dataset.from_tensor_slices((test_d, test_targets))
-
-BATCH_SIZE = 10
-SHUFFLE_BUFFER_SIZE = 100
-
-training_dataset = training_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-testing_dataset = testing_dataset.batch(BATCH_SIZE)
-
-#=======================================================================
-nb_layers = 4  # number of convolutional layers
-nb_filters = [64, 128, 128, 128]  # filter sizes
-kernel_size = (3, 3)  # convolution kernel size
-activation = 'elu'  # activation function to use after each layer
-pool_size = [(2, 2), (4, 2), (4, 2), (4, 2),
-             (4, 2)]  # size of pooling area
-
-# shape of input data (frequency, time, channels)
-inputs = tf.keras.Input(shape=[128,94,1])
-input_shape = (inputs.shape[1], inputs.shape[2], inputs.shape[3])
-frequency_axis = 1
-time_axis = 2
-channel_axis = 3
-
-# Create sequential model and normalize along frequency axis
-output_1 = layers.BatchNormalization(axis=frequency_axis, input_shape=input_shape)(inputs)
-
-# First convolution layer specifies shape
-output_1 = layers.Conv2D(nb_filters[0], kernel_size=kernel_size, padding='same',
-                         data_format="channels_last",
-                         input_shape=input_shape)(output_1)
-
-output_1 = layers.Activation(activation)(output_1)
-output_1 = layers.BatchNormalization(axis=channel_axis)(output_1)
-output_1 = layers.MaxPooling2D(pool_size=pool_size[0], strides=pool_size[0])(output_1)
-output_1 = layers.Dropout(0.1)(output_1)
-
-# Add more convolutional layers
-for layer in range(nb_layers - 1):
-    # Convolutional layer
-    output_1 = layers.Conv2D(nb_filters[layer + 1], kernel_size=kernel_size,
-                     padding='same')(output_1)
-    output_1 = layers.Activation(activation)(output_1)
-    output_1 = layers.BatchNormalization(
-        axis=channel_axis)(output_1)  # Improves overfitting/underfitting
-    output_1 = layers.MaxPooling2D(pool_size=pool_size[layer + 1],
-                           strides=pool_size[layer + 1])(output_1)  # Max pooling
-    output_1 = layers.Dropout(0.1)(output_1)
-
-    # Reshaping input for recurrent layer
-# (frequency, time, channels) --> (time, frequency, channel)
-output_1 = layers.Permute((time_axis, frequency_axis, channel_axis))(output_1)
-resize_shape = output_1.shape[2] * output_1.shape[3]
-output_1 = layers.Reshape((output_1.shape[1], resize_shape))(output_1)
-
-# recurrent layer
-output_1 = layers.GRU(32, return_sequences=True)(output_1)
-output_1 = layers.GRU(32, return_sequences=False)(output_1)
-output_1 = layers.Dropout(0.3)(output_1)
-
-# Output layer
-output_1 = layers.Dense(39)(output_1)
-output_1 = layers.Activation("softmax")(output_1)
-
-model = tf.keras.Model(inputs, output_1)
-model.summary()
-
-model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0005),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['sparse_categorical_accuracy'])
-
-# log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-history = model.fit(train_d, train_targets, epochs=1000, batch_size=16, validation_data=(x_val, y_val))
-
-model.evaluate(testing_dataset)
-
-print(history.history.keys())
-# summarize history for accuracy
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Training loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['loss', 'val loss'], loc='lower left')
-plt.show()
-# summarize history for loss plt.plot(history.history['loss']) plt.plot(history.history['val_loss']) plt.title('model loss')
-plt.plot(history.history['sparse_categorical_accuracy'])
-plt.plot(history.history['val_sparse_categorical_accuracy'])
-plt.title('Training accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train accuracy', 'val accuracy'], loc='upper left')
-plt.show()
-
-
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-#=======================================================================
-i = 2
-inputs = tf.keras.Input(shape=[128,94,1])
-
-temp1 = layers.Conv2D(i*10, (1,100), padding='same', activation='relu')(inputs)
-temp1 = layers.MaxPooling2D((4,temp1.shape[2]))(temp1)
-
-temp2 = layers.Conv2D(i*6, (3,100), padding='same', activation='relu')(inputs)
-temp2 = layers.MaxPooling2D((4,temp2.shape[2]))(temp2)
-
-temp3 = layers.Conv2D(i*3, (5,100), padding='same', activation='relu')(inputs)
-temp3 = layers.MaxPooling2D((4,temp3.shape[2]))(temp3)
-
-temp4 = layers.Conv2D(i*3, (7,100), padding='same', activation='relu')(inputs)
-temp4 = layers.MaxPooling2D((4,temp4.shape[2]))(temp4)
-
-temp5 = layers.Conv2D(i*15, (1,75), padding='same', activation='relu')(inputs)
-temp5 = layers.MaxPooling2D((4,temp5.shape[2]))(temp5)
-
-temp6 = layers.Conv2D(i*10, (3,75), padding='same', activation='relu')(inputs)
-temp6 = layers.MaxPooling2D((4,temp6.shape[2]))(temp6)
-
-temp7 = layers.Conv2D(i*5, (5,75), padding='same', activation='relu')(inputs)
-temp7 = layers.MaxPooling2D((4,temp7.shape[2]))(temp7)
-
-temp8 = layers.Conv2D(i*5, (7,75), padding='same', activation='relu')(inputs)
-temp8 = layers.MaxPooling2D((4,temp8.shape[2]))(temp8)
-
-temp9 = layers.Conv2D(i*15, (1,25), padding='same', activation='relu')(inputs)
-temp9 = layers.MaxPooling2D((4,temp9.shape[2]))(temp9)
-
-temp10 = layers.Conv2D(i*10, (3,25), padding='same', activation='relu')(inputs)
-temp10 = layers.MaxPooling2D((4,temp10.shape[2]))(temp10)
-
-temp11 = layers.Conv2D(i*5, (5,25), padding='same', activation='relu')(inputs)
-temp11 = layers.MaxPooling2D((4,temp11.shape[2]))(temp11)
-
-temp12 = layers.Conv2D(i*5, (7,25), padding='same', activation='relu')(inputs)
-temp12 = layers.MaxPooling2D((4,temp12.shape[2]))(temp12)
-
-temp = layers.Concatenate()([temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp7,temp8,temp9,temp10,temp11,temp12])
-temp = layers.Reshape((temp.shape[3],temp.shape[1],temp.shape[2]))(temp)
-temp = layers.Conv2D(i*16, (8,1), activation='relu')(temp)
-temp = layers.MaxPooling2D((4,1))(temp)
-temp = layers.Flatten()(temp)
-temp = layers.AlphaDropout(0.5)(temp)
-temp = layers.Dense(100, activation='relu')(temp)
-temp = layers.AlphaDropout(0.5)(temp)
-outputs = layers.Dense(39, activation='softmax')(temp)
-
-model = tf.keras.Model(inputs, outputs)
-model.summary()
-
-model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0005),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['sparse_categorical_accuracy'])
-
-model.fit(training_dataset, epochs=300)
-
-model.evaluate(testing_dataset)
-
-#=====================================================================================================================
-i = 2
-inputs = tf.keras.Input(shape=[128,94,1])
-frequency_axis = 1
-time_axis = 2
-channel_axis = 3
-
-temp1 = layers.Conv2D(i*10, (1,100), padding='same', activation='relu')(inputs)
-temp1 = layers.MaxPooling2D((4,temp1.shape[2]))(temp1)
-
-temp2 = layers.Conv2D(i*6, (3,100), padding='same', activation='relu')(inputs)
-temp2 = layers.MaxPooling2D((4,temp2.shape[2]))(temp2)
-
-temp3 = layers.Conv2D(i*3, (5,100), padding='same', activation='relu')(inputs)
-temp3 = layers.MaxPooling2D((4,temp3.shape[2]))(temp3)
-
-temp4 = layers.Conv2D(i*3, (7,100), padding='same', activation='relu')(inputs)
-temp4 = layers.MaxPooling2D((4,temp4.shape[2]))(temp4)
-
-temp5 = layers.Conv2D(i*15, (1,75), padding='same', activation='relu')(inputs)
-temp5 = layers.MaxPooling2D((4,temp5.shape[2]))(temp5)
-
-temp6 = layers.Conv2D(i*10, (3,75), padding='same', activation='relu')(inputs)
-temp6 = layers.MaxPooling2D((4,temp6.shape[2]))(temp6)
-
-temp7 = layers.Conv2D(i*5, (5,75), padding='same', activation='relu')(inputs)
-temp7 = layers.MaxPooling2D((4,temp7.shape[2]))(temp7)
-
-temp8 = layers.Conv2D(i*5, (7,75), padding='same', activation='relu')(inputs)
-temp8 = layers.MaxPooling2D((4,temp8.shape[2]))(temp8)
-
-temp9 = layers.Conv2D(i*15, (1,25), padding='same', activation='relu')(inputs)
-temp9 = layers.MaxPooling2D((4,temp9.shape[2]))(temp9)
-
-temp10 = layers.Conv2D(i*10, (3,25), padding='same', activation='relu')(inputs)
-temp10 = layers.MaxPooling2D((4,temp10.shape[2]))(temp10)
-
-temp11 = layers.Conv2D(i*5, (5,25), padding='same', activation='relu')(inputs)
-temp11 = layers.MaxPooling2D((4,temp11.shape[2]))(temp11)
-
-temp12 = layers.Conv2D(i*5, (7,25), padding='same', activation='relu')(inputs)
-temp12 = layers.MaxPooling2D((4,temp12.shape[2]))(temp12)
-
-temp = layers.Concatenate()([temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp7, temp8, temp9, temp10, temp11, temp12])
-
-resize_shape = temp.shape[2] * temp.shape[3]
-# temp = layers.Reshape((temp.shape[1], resize_shape))(temp)
-temp = layers.Reshape((temp.shape[3],temp.shape[1],temp.shape[2]))(temp)
-temp = layers.Conv2D(i*16, (8,1), activation='relu')(temp)
-output_1 = layers.MaxPooling2D((4,1))(temp)
-output_1 = layers.Reshape((output_1.shape[1],output_1.shape[2]*output_1.shape[3]))(output_1)
-
-# recurrent layer
-temp = layers.GRU(128, return_sequences=True)(output_1)
-temp = layers.GRU(128, return_sequences=False)(output_1)
-temp = layers.Dropout(0.3)(temp)
-
-outputs = layers.Dense(39, activation='softmax')(temp)
-
-model = tf.keras.Model(inputs, outputs)
-model.summary()
-# model.set_weights(weights['arr_2'])
-# config = model.get_config()
-
-model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0001),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['sparse_categorical_accuracy'])
-
-model.fit(training_dataset, epochs=100)
-
-model.evaluate(testing_dataset)
-
-model.save("first_trial",save_format='tf')
